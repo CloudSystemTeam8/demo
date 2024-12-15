527ef9081ba0f974ee3c7acd71e7b078a9027925
soyyeon <thdus8681@sookmyung.ac.kr>
2024-12-15 22:57:40 +0900

    get_datainterview 변경
    세션 테이블과 인터뷰 테이블을 join해 session_date가져오도록 get_datainterview 변경

--- iv-mate-server/sql.js
+++ iv-mate-server/sql.js
@@ -7,7 +7,6 @@ module.exports = {
   user_no_get: `SELECT user_no, nickname FROM user WHERE email = ?`,
   get_userinfo: `SELECT email, nickname FROM user WHERE user_no = ?`,
 
-  
   // AI
   get_jobinfo: `INSERT INTO SESSION (user_no, session_job, session_status) VALUES (?, ?, '진행중')`,
   get_sessionNo: `SELECT LAST_INSERT_ID() AS session_no`,
@@ -17,5 +16,5 @@ module.exports = {
   update_session: `UPDATE SESSION SET session_status = '끝남' WHERE session_no = ?`,
   get_airesult: `SELECT ai_result_no, ai_result_content FROM AIRESULT WHERE user_no = ? AND session_no = ? ORDER BY ai_result_no ASC`,
   get_sessiondate: `SELECT session_date FROM SESSION WHERE user_no = ? ORDER BY session_date DESC`,
-  get_dateinterview: `SELECT interview_question, interview_answer, interview_question_no FROM INTERVIEW WHERE user_no = ? AND session_date = ? ORDER BY interview_question_no ASC`,
+  get_dateinterview: `SELECT interview_question, interview_answer, interview_question_no, session_date FROM INTERVIEW i JOIN SESSION s ON i.session_no = s.session_no WHERE i.user_no = ? AND DATE(s.session_date) = ?`,
 };
